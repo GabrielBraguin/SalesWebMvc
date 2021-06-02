be1f78e1b5574d1d7f44c07b94f49c985e13605d
Gabriel Braguin <gabriel.braguin@hotmail.com.br>
2021-06-02 10:37:52 -0300

    date/amount format on sales record

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,14 +1,22 @@
 ﻿using SalesWebMvc.Models.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:F2}")] //currency format
         public double Amount { get; set; }
+
         public SaleStatus Status { get; set; }
+
         public Seller Seller { get; set; }
 
         public SalesRecord()
